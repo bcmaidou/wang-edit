97f51626cf182dee99cea5d618441ed51efe087c
bcmaidou@163.com <zhang1997>
2022-10-19 22:58:07 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <my-editor></my-editor>
+  </div>
+</template>
+
+<script>
+import myEditor from './components/MyEditor'
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
